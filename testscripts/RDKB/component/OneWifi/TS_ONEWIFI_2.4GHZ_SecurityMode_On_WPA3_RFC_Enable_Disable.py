466ffd580b09d14bca7be5d32106d40552a913f3
svanga970 <sharath_vangapalli@comcast.com>
2025-06-06 15:05:13 +0530

    TDK-10193: Revisit all scripts that involve WPA3 Personal modes
    Reason for change: Add BPI box type
    Test Procedure: Validated on RPI
    Risks: None
    
    Change-Id: I5395ccce2f6b58aba317562c6084c09fe02604b2
    Signed-off-by: svanga970 <sharath_vangapalli@comcast.com>

--- testscripts/RDKB/component/OneWifi/TS_ONEWIFI_2.4GHZ_SecurityMode_On_WPA3_RFC_Enable_Disable.py
+++ testscripts/RDKB/component/OneWifi/TS_ONEWIFI_2.4GHZ_SecurityMode_On_WPA3_RFC_Enable_Disable.py
@@ -43,6 +43,7 @@
   <!--  -->
   <box_types>
     <box_type>Broadband</box_type>
+    <box_type>BPI</box_type>
     <!--  -->
   </box_types>
   <rdk_versions>
@@ -51,7 +52,7 @@
   </rdk_versions>
   <test_cases>
     <test_case_id>TC_ONEWIFI_276</test_case_id>
-    <test_objective>To check the operational mode for private_wifi_2g (AP1) when WPA3_Personal_Transition.Enable is set to true.</test_objective>
+    <test_objective>Verify Device.WiFi.AccessPoint.1.Security.ModeEnabled when WPA3_Personal_Transition.Enable is set to both true and false.</test_objective>
     <test_type>Positive</test_type>
     <test_setup>Broadband</test_setup>
     <pre_requisite>1. Ccsp Components in DUT should be in a running state that includes component under test Cable Modem 2. TDK Agent should be in a running state or invoke it through StartTdk.sh script</pre_requisite>
@@ -59,15 +60,16 @@
     <input_parameters>Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.WPA3_Personal_Transition.Enable
 Device.WiFi.AccessPoint.1.Security.ModeEnabled</input_parameters>
     <automation_approach>
-      1. Load the module.
-      2. Get enable state of WPA3 RFC using Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.WPA3_Personal_Transition.Enable and set it to false.
-      3. If WPA3 RFC is enabled then security mode should be WPA3-Personal-Transition
-      4. Set WPA3_Personal_Transition.Enable using Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.WPA3_Personal_Transition.Enable and set it to true.
-      5. Get the security mode using Device.WiFi.AccessPoint.1.Security.ModeEnabled.
-      6. Disable WPA3_Personal_Transition.Enable if required.
-      7. Unload the module.
+1.Load the module.
+2.Perform Factory Reset on DUT
+3.Get WPA3 RFC using Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.WPA3_Personal_Transition.Enable and security mode using Device.WiFi.AccessPoint.1.Security.ModeEnabled.
+4.Enable WPA3 RFC using Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.WPA3_Personal_Transition.Enable
+5.Get WPA3 RFC using Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.WPA3_Personal_Transition.Enable and security mode using Device.WiFi.AccessPoint.1.Security.ModeEnabled.
+6.Disable WPA3 RFC using Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.WPA3_Personal_Transition.Enable.
+7.Unload the module.
     </automation_approach>
-    <expected_output>When WPA3_Personal_Transition.Enable is set to true, the operational mode for private_wifi_2g (AP1) should be verified correctly.</expected_output>
+    <expected_output>When WPA3_Personal_Transition.Enable is set to true, the security mode for Device.WiFi.AccessPoint.1.Security.ModeEnabled should be "WPA3-Personal-Transition".
+    And when WPA3_Personal_Transition.Enable is disabled from enabled state, the security mode should change to "WPA2-Personal"</expected_output>
     <priority>High</priority>
     <test_stub_interface>WifiAgent</test_stub_interface>
     <test_script>TS_ONEWIFI_2.4GHZ_SecurityMode_On_WPA3_RFC_Enable_Disable</test_script>
@@ -100,146 +102,121 @@ if "SUCCESS" in loadmodulestatus.upper():
     expectedresult = "SUCCESS"
     step = 1
 
-    # Step 1: Get the value of WPA3_Personal_Transition.Enable to verify it is false
-    paramName = "Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.WPA3_Personal_Transition.Enable"
-    tdkTestObj,actualresult,initial_rfc_wpa3 = wifi_GetParam(obj,paramName)
+    #Step 1: Do a Factory reset on DUT
+    #Save the current state before going to reboot
+    obj.saveCurrentState()
+    paramName_FR = "Device.X_CISCO_COM_DeviceControl.FactoryReset"
+    tdkTestObj,actualresult = wifi_SetParam(obj,paramName_FR,"Router,Wifi,VoIP,Dect,MoCA","string")
 
-    print("\nTEST STEP %s : Get the initial value of WPA3_Personal_Transition.Enable" % step)
-    print("EXPECTED RESULT %s : Should get the initial value of WPA3_Personal_Transition.Enable as false" % step)
-
-    if expectedresult in actualresult:
-        tdkTestObj.setResultStatus("SUCCESS")
-        print("ACTUAL RESULT %s: The value of initial WPA3_Personal_Transition.Enable is :%s" % (step,initial_rfc_wpa3))
-        print("TEST EXECUTION RESULT :SUCCESS")
-    else:
-        tdkTestObj.setResultStatus("FAILURE")
-        print("ACTUAL RESULT %s: The value of initial WPA3_Personal_Transition.Enable is  : %s" % (step,initial_rfc_wpa3))
-        print("TEST EXECUTION RESULT :FAILURE")
-    step +=step
-    # Step 2: Set WPA3_Personal_Transition.Enable to false
-    paramName = "Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.WPA3_Personal_Transition.Enable"
-    tdkTestObj,actualresult = wifi_SetParam(obj,paramName,"false","boolean")
-    sleep(1)
-
-    print("\nTEST STEP %s : Set WPA3_Personal_Transition.Enable to false" % step)
-    print("EXPECTED RESULT %s : Should successfully set WPA3_Personal_Transition.Enable to false" % step)
+    print(f"TEST STEP {step}: Initiate factory reset ")
+    print(f"EXPECTED RESULT {step}: Should initiate factory reset")
 
     if expectedresult in actualresult:
         tdkTestObj.setResultStatus("SUCCESS")
-        print("ACTUAL RESULT %s: RFC WPA3-Personal-Transition.Enable is set to false" % (step))
-        print("TEST EXECUTION RESULT :SUCCESS")
+        print(f"ACTUAL RESULT {step}: Factory Reset successfull")
+        print("TEST EXECUTION RESULT : SUCCESS")
+
+        #Restore the device state saved before reboot
+        obj.restorePreviousStateAfterReboot()
+        sleep(180)
+
+        #Step 2: Get WPA3RFC Enable and Security mode
+        step = step + 1
+        paramNames = [
+            "Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.WPA3_Personal_Transition.Enable",
+            "Device.WiFi.AccessPoint.1.Security.ModeEnabled"
+        ]
+        paramResults = {}
+        actualresult_all = []
+
+        for paramName in paramNames:
+            tdkTestObj, actualresult, paramValue = wifi_GetParam(obj, paramName)
+            actualresult_all.append(actualresult)
+            paramResults[paramName] = paramValue
+
+        rfc_wpa3 = paramResults["Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.WPA3_Personal_Transition.Enable"]
+        securityMode = paramResults["Device.WiFi.AccessPoint.1.Security.ModeEnabled"]
+
+        print(f"TEST STEP {step}: Get the value of WPA3_Personal_Transition.Enable and security mode")
+        print(f"EXPECTED RESULT {step}: Value of WPA3_Personal_Transition.Enable should be false and security mode should be WPA2-Personal")
+
+        if "FAILURE" not in actualresult_all and rfc_wpa3 == "false" and securityMode == "WPA2-Personal":
+            tdkTestObj.setResultStatus("SUCCESS")
+            print(f"ACTUAL RESULT {step}: WPA3_Personal_Transition.Enable is {rfc_wpa3} and security mode is {securityMode}")
+            print("TEST EXECUTION RESULT : SUCCESS")
+
+            #Step 3: Change WPA3_Personal_Transition.Enable to true
+            step = step+1
+            paramName = "Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.WPA3_Personal_Transition.Enable"
+            tdkTestObj,actualresult = wifi_SetParam(obj,paramName,"true","boolean")
+            sleep(2)
+
+            print(f"TEST STEP {step}: Change WPA3_Personal_Transition.Enable to true")
+            print(f"EXPECTED RESULT {step}: Should change WPA3_Personal_Transition.Enable to true")
+
+            if expectedresult in actualresult:
+                tdkTestObj.setResultStatus("SUCCESS")
+                print(f"ACTUAL RESULT {step}: SET operation SUCCESS")
+                print("TEST EXECUTION RESULT : SUCCESS")
+
+                #Step 4: Get WPA3RFC Enable and Security mode
+                step = step + 1
+                paramNames = [
+                    "Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.WPA3_Personal_Transition.Enable",
+                    "Device.WiFi.AccessPoint.1.Security.ModeEnabled"
+                ]
+                paramResults = {}
+                actualresult_all = []
+
+                for paramName in paramNames:
+                    tdkTestObj, actualresult, paramValue = wifi_GetParam(obj, paramName)
+                    actualresult_all.append(actualresult)
+                    paramResults[paramName] = paramValue
+
+                rfc_wpa3 = paramResults["Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.WPA3_Personal_Transition.Enable"]
+                securityMode = paramResults["Device.WiFi.AccessPoint.1.Security.ModeEnabled"]
+
+                print(f"TEST STEP {step}: Get the value of WPA3_Personal_Transition.Enable and security mode")
+                print(f"EXPECTED RESULT {step}: Value of WPA3_Personal_Transition.Enable should be true and security mode should be WPA3-Personal-Transition")
+
+                if "FAILURE" not in actualresult_all and rfc_wpa3 == "true" and securityMode == "WPA3-Personal-Transition":
+                    tdkTestObj.setResultStatus("SUCCESS")
+                    print(f"ACTUAL RESULT {step}: WPA3_Personal_Transition.Enable is {rfc_wpa3} and security mode is {securityMode}")
+                    print("TEST EXECUTION RESULT : SUCCESS")
+
+                    #Step 5: Revert WPA3-Personal-Transition to false
+                    step = step+1
+                    paramName = "Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.WPA3_Personal_Transition.Enable"
+                    tdkTestObj,actualresult = wifi_SetParam(obj,paramName,"false","boolean")
+                    sleep(2)
+
+                    print(f"TEST STEP {step}: Revert WPA3_Personal_Transition.Enable to false")
+                    print(f"EXPECTED RESULT {step}: Should revert WPA3_Personal_Transition.Enable to false")
+
+                    if expectedresult in actualresult:
+                        tdkTestObj.setResultStatus("SUCCESS")
+                        print(f"ACTUAL RESULT {step}: SET operation SUCCESS")
+                        print("TEST EXECUTION RESULT : SUCCESS")
+                    else:
+                        tdkTestObj.setResultStatus("FAILURE")
+                        print(f"ACTUAL RESULT {step}: SET operation FAILURE")
+                        print("TEST EXECUTION RESULT : FAILURE")
+                else:
+                    tdkTestObj.setResultStatus("FAILURE")
+                    print(f"ACTUAL RESULT {step}: WPA3_Personal_Transition.Enable is {rfc_wpa3} and security mode is {securityMode}")
+                    print("TEST EXECUTION RESULT : FAILURE")
+            else:
+                tdkTestObj.setResultStatus("FAILURE")
+                print(f"ACTUAL RESULT {step}: SET operation FAILURE")
+                print("TEST EXECUTION RESULT : FAILURE")
+        else:
+            tdkTestObj.setResultStatus("FAILURE")
+            print(f"ACTUAL RESULT {step}: WPA3_Personal_Transition.Enable is {rfc_wpa3} and security mode is {securityMode}")
+            print("TEST EXECUTION RESULT : FAILURE")
     else:
         tdkTestObj.setResultStatus("FAILURE")
-        print("ACTUAL RESULT %s: Failed to set RFC WPA3-Personal-Transition.Enable to false" % (step))
-        print("TEST EXECUTION RESULT :FAILURE")
-
-    step += 1
-
-    # Step 3: Get the value of WPA3_Personal_Transition.Enable to verify it is false
-    paramName = "Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.WPA3_Personal_Transition.Enable"
-    tdkTestObj,actualresult,rfc_wpa3 = wifi_GetParam(obj,paramName)
-
-    print("\nTEST STEP %s : Get the value of WPA3_Personal_Transition.Enable" % step)
-    print("EXPECTED RESULT %s : Should get the value of WPA3_Personal_Transition.Enable as false" % step)
-
-    if expectedresult in actualresult and rfc_wpa3 == "false":
-        tdkTestObj.setResultStatus("SUCCESS")
-        print("ACTUAL RESULT %s: The value of WPA3_Personal_Transition.Enable is :%s" % (step,rfc_wpa3))
-        print("TEST EXECUTION RESULT :SUCCESS")
-    else:
-        tdkTestObj.setResultStatus("FAILURE")
-        print("ACTUAL RESULT %s: The value of WPA3_Personal_Transition.Enable is  : %s" % (step,rfc_wpa3))
-        print("TEST EXECUTION RESULT :FAILURE")
-
-    step += 1
-
-    # Step 4: Get the current security mode
-    paramName = "Device.WiFi.AccessPoint.1.Security.ModeEnabled"
-    tdkTestObj,actualresult,securityMode = wifi_GetParam(obj,paramName)
-
-    print("\nTEST STEP %s : Check the SecurityMode" % step)
-    print("EXPECTED RESULT %s : SecurityMode should be WPA2-Personal when WPA3_Personal_Transition.Enable is false  " % step)
-
-    if expectedresult in actualresult and securityMode == "WPA2-Personal":
-        tdkTestObj.setResultStatus("SUCCESS")
-        print("ACTUAL RESULT %s: SecurityMode is : %s" % (step, securityMode))
-        print("TEST EXECUTION RESULT :SUCCESS")
-    else:
-        tdkTestObj.setResultStatus("FAILURE")
-        print("ACTUAL RESULT %s: SecurityMode is  : %s" % (step, securityMode))
-        print("TEST EXECUTION RESULT :FAILURE")
-
-    step += 1
-
-    # Step 5: Set WPA3_Personal_Transition.Enable to true
-    paramName = "Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.WPA3_Personal_Transition.Enable"
-    tdkTestObj,actualresult = wifi_SetParam(obj,paramName,"true","boolean")
-    sleep(1)
-
-    print("\nTEST STEP %s : Set WPA3_Personal_Transition.Enable to true" % step)
-    print("EXPECTED RESULT %s : Should successfully set WPA3_Personal_Transition.Enable to true" % step)
-
-    if expectedresult in actualresult:
-        tdkTestObj.setResultStatus("SUCCESS")
-        print("ACTUAL RESULT %s: The value of WPA3_Personal_Transition.Enable is set to true " % (step))
-        print("TEST EXECUTION RESULT :SUCCESS")
-    else:
-        tdkTestObj.setResultStatus("FAILURE")
-        print("ACTUAL RESULT %s: Failed to set value of WPA3_Personal_Transition.Enable to true %s" % (step, rfc_wpa3))
-        print("TEST EXECUTION RESULT :FAILURE")
-
-    step += 1
-
-    # Step 6: Get the value of WPA3_Personal_Transition.Enable to verify it is true
-    paramName = "Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.WPA3_Personal_Transition.Enable"
-    tdkTestObj,actualresult,rfc_wpa3 = wifi_GetParam(obj,paramName)
-
-    print("\nTEST STEP %s : Get the value of WPA3_Personal_Transition.Enable" % step)
-    print("EXPECTED RESULT %s : Should get the value of WPA3_Personal_Transition.Enable as true" % step)
-
-    if expectedresult in actualresult and rfc_wpa3 == "true":
-        tdkTestObj.setResultStatus("SUCCESS")
-        print("ACTUAL RESULT %s: The value of WPA3_Personal_Transition.Enable is: %s" % (step, rfc_wpa3))
-        print("TEST EXECUTION RESULT :SUCCESS")
-    else:
-        tdkTestObj.setResultStatus("FAILURE")
-        print("ACTUAL RESULT %s: The value of WPA3_Personal_Transition.Enable is : %s" % (step, rfc_wpa3))
-        print("TEST EXECUTION RESULT :FAILURE")
-
-    step += 1
-
-    # Step 7: Get the security mode again to verify it has changed
-    paramName = "Device.WiFi.AccessPoint.1.Security.ModeEnabled"
-    tdkTestObj,actualresult,newSecurityMode = wifi_GetParam(obj,paramName)
-
-    print("\nTEST STEP %d : Check security mode after enabling RFC WPA3_Personal_Transition" % step)
-    print("EXPECTED RESULT %d : Security mode should be WPA3-Personal-Transition after enabling RFC WPA3_Personal_Transition " % step)
-
-    if expectedresult in actualresult and newSecurityMode == "WPA3-Personal-Transition":
-        tdkTestObj.setResultStatus("SUCCESS")
-        print("ACTUAL RESULT %d: Security Mode after enabling RFC WPA3_Personal_Transition %s" % (step, newSecurityMode))
-        print("TEST EXECUTION RESULT :SUCCESS")
-    else:
-        tdkTestObj.setResultStatus("FAILURE")
-        print("ACTUAL RESULT %d: Security Mode after enabling RFC WPA3_Personal_Transition %s" % (step, newSecurityMode))
-        print("TEST EXECUTION RESULT :FAILURE")
-
-    step += 1
-    # Step 8: Revert WPA3_Personal_Transition.Enable back to initial value
-    paramName = "Device.DeviceInfo.X_RDKCENTRAL-COM_RFC.Feature.WPA3_Personal_Transition.Enable"
-    tdkTestObj,actualresult = wifi_SetParam(obj,paramName,initial_rfc_wpa3,"boolean")
-
-    print("\nTEST STEP %d : Set WPA3_Personal_Transition.Enable back to initial value" % step)
-    print("EXPECTED RESULT %d : Should successfully set WPA3_Personal_Transition.Enable back to initial value" % step)
-
-    if expectedresult in actualresult:
-        tdkTestObj.setResultStatus("SUCCESS")
-        print("ACTUAL RESULT %d: The value of WPA3_Personal_Transition.Enable is set to: %s" % (step,initial_rfc_wpa3))
-        print("TEST EXECUTION RESULT :SUCCESS")
-    else:
-        tdkTestObj.setResultStatus("FAILURE")
-        print("ACTUAL RESULT %d: Failed to set the value of WPA3_Personal_Transition.Enable to initial value: %s" % (step))
-        print("TEST EXECUTION RESULT :FAILURE")
+        print(f"ACTUAL RESULT {step}: Factory Reset FAILURE")
+        print("TEST EXECUTION RESULT : FAILURE")
 
     obj.unloadModule("wifiagent")
 else:
