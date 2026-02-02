0df4c6dd4a6144658c40045dfe52657e719c8400
LekshmiSRaj363 <lekshmis.r@tataelxsi.co.in>
2026-02-02 15:33:11 +0530

    TDK-11214 TDK Coverage for RNDIS
    Reason for change: TDK Coverage for RNDIS Feature
    Test Procedure: Run scripts in BPI
    Risks: None

--- testscripts/RDKB/component/RNDIS/TS_RNDIS_VerifyHostTableUpdateWithLANClient.py
+++ testscripts/RDKB/component/RNDIS/TS_RNDIS_VerifyHostTableUpdateWithLANClient.py
@@ -67,66 +67,9 @@ if "SUCCESS" in loadmodulestatus_sys.upper() and "SUCCESS" in loadmodulestatus.u
                 print("[TEST EXECUTION RESULT] : SUCCESS")
 
                 # Step 3 & 4: Iterate through the Host Table and find LAN client entry
-                clientDetected = 0
-                clientIndex = -1
-                for index in range(1, hostEntries + 1):
-                    print("\n**********For Host Table Entry %d**********" % index)
+                tdkTestObj_tr181, actualresult, clientDetected, clientIndex, detectedInfo, step = traverse_host_table_for_client(obj, hostEntries, "LAN", step)
 
-                    step += 1
-                    # Get the value of Device.Hosts.Host.{i}.Layer1Interface
-                    paramName = "Device.Hosts.Host." + str(index) + ".Layer1Interface"
-                    print("\nTEST STEP %d: Get the value of %s and check if it is Ethernet" % (step, paramName))
-                    print("EXPECTED RESULT %d: Should successfully retrieve %s" % (step, paramName))
-                    tdkTestObj_tr181 = obj.createTestStep('TDKB_TR181Stub_Get')
-                    actualresult, details = getTR181Value(tdkTestObj_tr181, paramName)
-
-                    if expectedresult in actualresult:
-                        layer1Interface = details.split("VALUE:")[1].split(',')[0].strip() if "VALUE:" in details else details.strip()
-                        tdkTestObj_tr181.setResultStatus("SUCCESS")
-                        print("ACTUAL RESULT %d: %s : %s" % (step, paramName, layer1Interface))
-                        print("[TEST EXECUTION RESULT] : SUCCESS")
-                        if layer1Interface == EXPECTED_LAYER1_INTERFACE_ETHERNET:
-                            clientDetected = 1
-                            clientIndex = index
-                            print("Identified the Host Table Entry for LAN client as : %d" % index)
-
-                            # Step 5: Check if the LAN client is shown as active
-                            step += 1
-                            paramName = "Device.Hosts.Host." + str(index) + ".Active"
-                            print("\nTEST STEP %d: Get the value of %s and check if it is true" % (step, paramName))
-                            print("EXPECTED RESULT %d: Should successfully retrieve %s and it should be true" % (step, paramName))
-                            tdkTestObj_tr181 = obj.createTestStep('TDKB_TR181Stub_Get')
-                            actualresult, details = getTR181Value(tdkTestObj_tr181, paramName)
-
-                            if expectedresult in actualresult:
-                                activeStatus = details.split("VALUE:")[1].split(',')[0].strip() if "VALUE:" in details else details.strip()
-                                tdkTestObj_tr181.setResultStatus("SUCCESS")
-                                print("ACTUAL RESULT %d: %s : %s" % (step, paramName, activeStatus))
-                                print("[TEST EXECUTION RESULT] : SUCCESS")
-
-                                if activeStatus == "true":
-                                    tdkTestObj_tr181.setResultStatus("SUCCESS")
-                                    print("Host is Active - LAN client detected in Host Table while RNDIS is active")
-                                    break
-                                else:
-                                    tdkTestObj_tr181.setResultStatus("FAILURE")
-                                    print("Host is NOT Active")
-                                    break
-                            else:
-                                tdkTestObj_tr181.setResultStatus("FAILURE")
-                                print("ACTUAL RESULT %d: Failed to get %s. Details: %s" % (step, paramName, details))
-                                print("[TEST EXECUTION RESULT] : FAILURE")
-                                break
-                        else:
-                            print("Host Table Entry %d Layer1Interface is %s (not Ethernet)" % (index, layer1Interface))
-                            continue
-                    else:
-                        tdkTestObj_tr181.setResultStatus("FAILURE")
-                        print("ACTUAL RESULT %d: Failed to get %s. Details: %s" % (step, paramName, details))
-                        print("[TEST EXECUTION RESULT] : FAILURE")
-                        break
-
-                # Step 6: Check if client was detected
+                # Step 5: Check if client was detected
                 if clientDetected != 1:
                     step += 1
                     tdkTestObj_tr181.setResultStatus("FAILURE")
--- testscripts/RDKB/component/RNDIS/TS_RNDIS_VerifyHostTableUpdateWithWiFiClient.py
+++ testscripts/RDKB/component/RNDIS/TS_RNDIS_VerifyHostTableUpdateWithWiFiClient.py
@@ -68,71 +68,9 @@ if "SUCCESS" in loadmodulestatus_sys.upper() and "SUCCESS" in loadmodulestatus.u
                 print("[TEST EXECUTION RESULT] : SUCCESS")
 
                 # Step 3 & 4: Iterate through the Host Table and find WiFi client entry
-                clientDetected = 0
-                clientIndex = -1
-                detectedSSID = ""
-                for index in range(1, hostEntries + 1):
-                    print("\n**********For Host Table Entry %d**********" % index)
+                tdkTestObj_tr181, actualresult, clientDetected, clientIndex, detectedSSID, step = traverse_host_table_for_client(obj, hostEntries, "WiFi", step)
 
-                    step += 1
-                    # Get the value of Device.Hosts.Host.{i}.Layer1Interface
-                    paramName = "Device.Hosts.Host." + str(index) + ".Layer1Interface"
-                    print("\nTEST STEP %d: Get the value of %s and check if it is Device.WiFi.SSID.1 or Device.WiFi.SSID.2" % (step, paramName))
-                    print("EXPECTED RESULT %d: Should successfully retrieve %s" % (step, paramName))
-                    tdkTestObj_tr181 = obj.createTestStep('TDKB_TR181Stub_Get')
-                    actualresult, details = getTR181Value(tdkTestObj_tr181, paramName)
-
-                    if expectedresult in actualresult:
-                        layer1Interface = details.split("VALUE:")[1].split(',')[0].strip() if "VALUE:" in details else details.strip()
-                        tdkTestObj_tr181.setResultStatus("SUCCESS")
-                        print("ACTUAL RESULT %d: %s : %s" % (step, paramName, layer1Interface))
-                        print("[TEST EXECUTION RESULT] : SUCCESS")
-
-                        # Check if Layer1Interface is either Device.WiFi.SSID.1 or Device.WiFi.SSID.2
-                        if layer1Interface == EXPECTED_LAYER1_INTERFACE_WIFI_SSID1 or layer1Interface == EXPECTED_LAYER1_INTERFACE_WIFI_SSID2:
-                            clientDetected = 1
-                            clientIndex = index
-                            detectedSSID = layer1Interface
-                            print("Identified the Host Table Entry for WiFi client as : %d" % index)
-                            print("WiFi client connected to : %s" % layer1Interface)
-
-                            # Step 5: Check if the WiFi client is shown as active
-                            step += 1
-                            paramName = "Device.Hosts.Host." + str(index) + ".Active"
-                            print("\nTEST STEP %d: Get the value of %s and check if it is true" % (step, paramName))
-                            print("EXPECTED RESULT %d: Should successfully retrieve %s and it should be true" % (step, paramName))
-                            tdkTestObj_tr181 = obj.createTestStep('TDKB_TR181Stub_Get')
-                            actualresult, details = getTR181Value(tdkTestObj_tr181, paramName)
-
-                            if expectedresult in actualresult:
-                                activeStatus = details.split("VALUE:")[1].split(',')[0].strip() if "VALUE:" in details else details.strip()
-                                tdkTestObj_tr181.setResultStatus("SUCCESS")
-                                print("ACTUAL RESULT %d: %s : %s" % (step, paramName, activeStatus))
-                                print("[TEST EXECUTION RESULT] : SUCCESS")
-
-                                if activeStatus == "true":
-                                    tdkTestObj_tr181.setResultStatus("SUCCESS")
-                                    print("Host is Active - WiFi client detected in Host Table (%s) while RNDIS is active" % detectedSSID)
-                                    break
-                                else:
-                                    tdkTestObj_tr181.setResultStatus("FAILURE")
-                                    print("Host is NOT Active")
-                                    break
-                            else:
-                                tdkTestObj_tr181.setResultStatus("FAILURE")
-                                print("ACTUAL RESULT %d: Failed to get %s. Details: %s" % (step, paramName, details))
-                                print("[TEST EXECUTION RESULT] : FAILURE")
-                                break
-                        else:
-                            print("Host Table Entry %d Layer1Interface is %s (not Device.WiFi.SSID.1 or Device.WiFi.SSID.2)" % (index, layer1Interface))
-                            continue
-                    else:
-                        tdkTestObj_tr181.setResultStatus("FAILURE")
-                        print("ACTUAL RESULT %d: Failed to get %s. Details: %s" % (step, paramName, details))
-                        print("[TEST EXECUTION RESULT] : FAILURE")
-                        break
-
-                # Step 6: Check if client was detected
+                # Step 5: Check if client was detected
                 if clientDetected != 1:
                     step += 1
                     tdkTestObj_tr181.setResultStatus("FAILURE")
