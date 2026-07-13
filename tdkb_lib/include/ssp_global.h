94c86c4cc535e4ced3bbb151bbaa200a3220cd2b
Anjali V <anjali.v@tataelxsi.co.in>
2026-07-13 15:40:57 +0530

    Removing all linux/user_base.h dependencies

--- tdkb_lib/include/ssp_global.h
+++ tdkb_lib/include/ssp_global.h
@@ -36,7 +36,7 @@
 #define  _SSP_GLOBAL_
 #include <time.h>
 //#include "ansc_platform.h"
-#include "slap_definitions.h"
+//#include "slap_definitions.h"
 //#include "ccsp_message_bus.h"
 #include "ccsp_base_api.h"
 #include "ccsp_trace.h"
@@ -44,7 +44,7 @@
 #include "dslh_cpeco_interface.h"
 #include "dslh_cpeco_exported_api.h"
 #include "ccsp_component_helper.h"
-//#include "slap_vco_exported_api.h"
+#include "slap_vco_exported_api.h"
 #include "ssp_messagebus_interface.h"
 #include "dslh_ifo_mpa.h"
 #include "dslh_dmagnt_interface.h"
