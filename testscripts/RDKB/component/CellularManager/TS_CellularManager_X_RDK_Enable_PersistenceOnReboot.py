23e526a1e57b112046ec1534f521903c796383a9
44512 <juhanalex.z@tataelxsi.co.in>
2026-07-29 18:17:48 +0530

    TDK-12266: Fix X_RDK_Enable persistence validation logic

--- testscripts/RDKB/component/CellularManager/TS_CellularManager_X_RDK_Enable_PersistenceOnReboot.py
+++ testscripts/RDKB/component/CellularManager/TS_CellularManager_X_RDK_Enable_PersistenceOnReboot.py
@@ -30,25 +30,29 @@ obj = tdklib.TDKScriptingLibrary("tdkbtr181","1");
 #This will be replaced with corresponding DUT Ip and port while executing script
 ip = <ipaddress>
 port = <port>
-sysobj.configureTestCase(ip,port,'TS_CellularManager_X_RDK_EnablePersistenceOnReboot');
-obj.configureTestCase(ip,port,'TS_CellularManager_X_RDK_EnablePersistenceOnReboot');
+sysobj.configureTestCase(ip,port,'TS_CellularManager_X_RDK_Enable_PersistenceOnReboot');
+obj.configureTestCase(ip,port,'TS_CellularManager_X_RDK_Enable_PersistenceOnReboot');
 
 #Get the result of connection with test component and DUT
-loadmodulestatus_sys =sysobj.getLoadModuleResult();
-loadmodulestatus =obj.getLoadModuleResult();
-print ("[LIB LOAD STATUS]  :  %s" %loadmodulestatus_sys);
-print ("[LIB LOAD STATUS]  :  %s" %loadmodulestatus);
+loadmodulestatus_sys = sysobj.getLoadModuleResult();
+loadmodulestatus = obj.getLoadModuleResult();
 
-print("Loading Cellular Manager module")
-if "SUCCESS" in loadmodulestatus.upper() and loadmodulestatus_sys.upper():
+print("[LIB LOAD STATUS]  :  %s" %loadmodulestatus_sys);
+print("[LIB LOAD STATUS]  :  %s" %loadmodulestatus);
+
+print("Loading Cellular Manager module");
+
+if "SUCCESS" in loadmodulestatus.upper() and "SUCCESS" in loadmodulestatus_sys.upper():
+
     sysobj.setLoadModuleStatus("SUCCESS");
     obj.setLoadModuleStatus("SUCCESS");
 
+    expectedresult = "SUCCESS";
     step = 1;
 
-############################################################
-# STEP 1 : Verify Device.Cellular.Interface.1.Enable
-############################################################
+    ############################################################
+    # STEP 1 : Get Device.Cellular.Interface.1.Enable
+    ############################################################
 
     tdkTestObj = obj.createTestStep('TDKB_TR181Stub_Get');
     tdkTestObj.addParameter(
@@ -56,18 +60,16 @@ if "SUCCESS" in loadmodulestatus.upper() and loadmodulestatus_sys.upper():
         "Device.Cellular.Interface.1.Enable"
     );
 
-    expectedresult = "SUCCESS";
-
     tdkTestObj.executeTestCase(expectedresult);
 
     actualresult = tdkTestObj.getResult();
     interfaceEnable = tdkTestObj.getResultDetails().strip();
 
     print("TEST STEP %d: Get Device.Cellular.Interface.1.Enable" %step);
-    print("EXPECTED RESULT %d: Device.Cellular.Interface.1.Enable should be true" %step);
+    print("EXPECTED RESULT %d: Should get Device.Cellular.Interface.1.Enable value" %step);
     print("ACTUAL RESULT %d: Value is %s" %(step,interfaceEnable));
 
-    if expectedresult in actualresult and interfaceEnable == "true":
+    if expectedresult in actualresult and interfaceEnable != "":
 
         tdkTestObj.setResultStatus("SUCCESS");
         print("[TEST EXECUTION RESULT] : SUCCESS");
@@ -77,11 +79,11 @@ if "SUCCESS" in loadmodulestatus.upper() and loadmodulestatus_sys.upper():
         tdkTestObj.setResultStatus("FAILURE");
         print("[TEST EXECUTION RESULT] : FAILURE");
 
-    step = step + 1;
+    ############################################################
+    # STEP 2 : Get Device.Cellular.X_RDK_Status
+    ############################################################
 
-############################################################
-# STEP 2 : Verify Device.Cellular.X_RDK_Status
-############################################################
+    step = step + 1;
 
     tdkTestObj = obj.createTestStep('TDKB_TR181Stub_Get');
     tdkTestObj.addParameter(
@@ -92,13 +94,13 @@ if "SUCCESS" in loadmodulestatus.upper() and loadmodulestatus_sys.upper():
     tdkTestObj.executeTestCase(expectedresult);
 
     actualresult = tdkTestObj.getResult();
-    status = tdkTestObj.getResultDetails();
+    status = tdkTestObj.getResultDetails().strip();
 
-    print("TEST STEP %d: Verify Device.Cellular.X_RDK_Status" %step);
-    print("EXPECTED RESULT %d: Device.Cellular.X_RDK_Status should be CONNECTED" %step);
+    print("TEST STEP %d: Get Device.Cellular.X_RDK_Status" %step);
+    print("EXPECTED RESULT %d: Should get Device.Cellular.X_RDK_Status value" %step);
     print("ACTUAL RESULT %d: Status is %s" %(step,status));
 
-    if expectedresult in actualresult and status == "CONNECTED":
+    if expectedresult in actualresult and status != "":
 
         tdkTestObj.setResultStatus("SUCCESS");
         print("[TEST EXECUTION RESULT] : SUCCESS");
@@ -107,120 +109,254 @@ if "SUCCESS" in loadmodulestatus.upper() and loadmodulestatus_sys.upper():
 
         tdkTestObj.setResultStatus("FAILURE");
         print("[TEST EXECUTION RESULT] : FAILURE");
+
+    ############################################################
+    # STEP 3 : Get Device.Cellular.X_RDK_Enable
+    ############################################################
 
     step = step + 1;
 
     tdkTestObj = obj.createTestStep('TDKB_TR181Stub_Get');
-    tdkTestObj.addParameter("ParamName","Device.Cellular.X_RDK_Enable");
-    expectedresult="SUCCESS";
+    tdkTestObj.addParameter(
+        "ParamName",
+        "Device.Cellular.X_RDK_Enable"
+    );
 
-    #Execute testcase in DUT
     tdkTestObj.executeTestCase(expectedresult);
+
     actualresult = tdkTestObj.getResult();
-    initialvalue = tdkTestObj.getResultDetails();
-    print("TEST STEP %d: Get the Device.Cellular.X_RDK_Enable" %step);
-    print("EXPECTED RESULT %d: Should get the Device.Cellular.X_RDK_Enable" %step);
+    initialvalue = tdkTestObj.getResultDetails().strip();
+
+    print("TEST STEP %d: Get Device.Cellular.X_RDK_Enable" %step);
+    print("EXPECTED RESULT %d: Should get Device.Cellular.X_RDK_Enable" %step);
     print("ACTUAL RESULT %d: Value is %s" %(step,initialvalue));
 
-    if expectedresult in actualresult and initialvalue != "":
-        #Set the result status of execution
+    if expectedresult in actualresult and initialvalue in ["true", "false"]:
+
         tdkTestObj.setResultStatus("SUCCESS");
-        #Get the result of execution
         print("[TEST EXECUTION RESULT] : SUCCESS");
 
+        ############################################################
+        # STEP 4 : Set Device.Cellular.X_RDK_Enable to false
+        ############################################################
 
         step = step + 1;
+
         setVal = "false";
+
         tdkTestObj = obj.createTestStep('TDKB_TR181Stub_Set');
-        tdkTestObj.addParameter("ParamName","Device.Cellular.X_RDK_Enable");
-        tdkTestObj.addParameter("ParamValue",setVal);
-        tdkTestObj.addParameter("Type","bool");
-        expectedresult="SUCCESS";
+        tdkTestObj.addParameter(
+            "ParamName",
+            "Device.Cellular.X_RDK_Enable"
+        );
+        tdkTestObj.addParameter(
+            "ParamValue",
+            setVal
+        );
+        tdkTestObj.addParameter(
+            "Type",
+            "bool"
+        );
 
-        #Execute testcase in DUT
         tdkTestObj.executeTestCase(expectedresult);
-        actualresult = tdkTestObj.getResult();
-        setvalue = tdkTestObj.getResultDetails();
 
-        print("TEST STEP %d: Set the Device.Cellular.X_RDK_Enable" %step);
-        print("EXPECTED RESULT %d: Should set the Device.Cellular.X_RDK_Enable as %s" %(step,setVal));
-        print("ACTUAL RESULT %d: Value is %s" %(step,setvalue));
+        actualresult = tdkTestObj.getResult();
+        setDetails = tdkTestObj.getResultDetails();
+
+        print("TEST STEP %d: Set Device.Cellular.X_RDK_Enable" %step);
+        print("EXPECTED RESULT %d: Should set Device.Cellular.X_RDK_Enable as %s" %(step,setVal));
+        print("ACTUAL RESULT %d: Value is %s" %(step,setDetails));
 
         if expectedresult in actualresult:
-            #Set the result status of execution
+
             tdkTestObj.setResultStatus("SUCCESS");
-            #Get the result of execution
             print("[TEST EXECUTION RESULT] : SUCCESS");
 
-            #rebooting the device
+        else:
+
+            print("Set operation did not return SUCCESS, verifying parameter value using GET");
+
+        sleep(10);
+
+        ############################################################
+        # STEP 5 : Verify Device.Cellular.X_RDK_Enable after Set
+        ############################################################
+
+        step = step + 1;
+
+        tdkTestObj = obj.createTestStep('TDKB_TR181Stub_Get');
+        tdkTestObj.addParameter(
+            "ParamName",
+            "Device.Cellular.X_RDK_Enable"
+        );
+
+        tdkTestObj.executeTestCase(expectedresult);
+
+        actualresult = tdkTestObj.getResult();
+        getAfterSet = tdkTestObj.getResultDetails().strip();
+
+        print("TEST STEP %d: Verify Device.Cellular.X_RDK_Enable after Set" %step);
+        print("EXPECTED RESULT %d: Device.Cellular.X_RDK_Enable should be %s" %(step,setVal));
+        print("ACTUAL RESULT %d: Value is %s" %(step,getAfterSet));
+
+        if expectedresult in actualresult and getAfterSet == setVal:
+
+            tdkTestObj.setResultStatus("SUCCESS");
+            print("[TEST EXECUTION RESULT] : SUCCESS");
+
+            ############################################################
+            # STEP 6 : Reboot DUT
+            ############################################################
+
+            step = step + 1;
+
+            print("TEST STEP %d: Reboot the DUT" %step);
+            print("EXPECTED RESULT %d: DUT should reboot successfully" %step);
+
             sysobj.initiateReboot();
             sleep(60);
 
-            tdkTestObj = obj.createTestStep('TDKB_TR181Stub_Get');
-            tdkTestObj.addParameter("ParamName","Device.Cellular.X_RDK_Enable");
-            expectedresult="SUCCESS";
+            print("ACTUAL RESULT %d: DUT reboot completed" %step);
+            print("[TEST EXECUTION RESULT] : SUCCESS");
+
+            ############################################################
+            # STEP 7 : Get Device.Cellular.X_RDK_Enable after Reboot
+            ############################################################
 
             step = step + 1;
-            #Execute testcase in DUT
+
+            tdkTestObj = obj.createTestStep('TDKB_TR181Stub_Get');
+            tdkTestObj.addParameter(
+                "ParamName",
+                "Device.Cellular.X_RDK_Enable"
+            );
+
             tdkTestObj.executeTestCase(expectedresult);
+
             actualresult = tdkTestObj.getResult();
-            newvalue = tdkTestObj.getResultDetails();
-            print("TEST STEP %d: Get the Device.Cellular.X_RDK_Enable after Reboot" %step);
-            print("EXPECTED RESULT %d:  Should get the  Device.Cellular.X_RDK_Enable value " %step);
+            newvalue = tdkTestObj.getResultDetails().strip();
+
+            print("TEST STEP %d: Get Device.Cellular.X_RDK_Enable after Reboot" %step);
+            print("EXPECTED RESULT %d: Should get Device.Cellular.X_RDK_Enable value" %step);
             print("ACTUAL RESULT %d: Value is %s" %(step,newvalue));
 
             if expectedresult in actualresult and newvalue != "":
-                #Set the result status of execution
+
                 tdkTestObj.setResultStatus("SUCCESS");
-                #Get the result of execution
                 print("[TEST EXECUTION RESULT] : SUCCESS");
 
-                print("TEST STEP %d: Check persistence of Device.Cellular.X_RDK_Enable value after Reboot"%step);
-                print("EXPECTED RESULT %d:  The value of Device.Cellular.X_RDK_Enable should persist after reboot"%step);
+                ############################################################
+                # STEP 8 : Check Persistence after Reboot
+                ############################################################
+
+                step = step + 1;
+
+                print("TEST STEP %d: Check persistence of Device.Cellular.X_RDK_Enable value after Reboot" %step);
+                print("EXPECTED RESULT %d: Device.Cellular.X_RDK_Enable should persist as %s after reboot" %(step,setVal));
                 print("ACTUAL RESULT %d: Value is %s" %(step,newvalue));
 
-                #Check if the value persists after reboot
-                if newvalue == setvalue :
-                    #Set the result status of execution
-                    tdkTestObj.setResultStatus("SUCCESS");
-                    #Get the result of execution
-                    print("[TEST EXECUTION RESULT] : SUCCESS");
-                else:
-                    #Set the result status of execution
-                    tdkTestObj.setResultStatus("FAILURE");
-                    #Get the result of execution
-                    print("[TEST EXECUTION RESULT] : FAILURE");
-            else:
-                #Set the result status of execution
-                tdkTestObj.setResultStatus("FAILURE");
-                #Get the result of execution
-                print("[TEST EXECUTION RESULT] : FAILURE");
-            #Reverting the value of Device.Cellular.X_RDK_Enable to initial one
-            tdkTestObj = obj.createTestStep('TDKB_TR181Stub_Set');
-            tdkTestObj.addParameter("ParamName","Device.Cellular.X_RDK_Enable");
-            tdkTestObj.addParameter("ParamValue",initialvalue);
-            tdkTestObj.addParameter("Type","bool");
-            expectedresult="SUCCESS";
+                if newvalue == setVal:
 
-            #Execute testcase in DUT
+                    tdkTestObj.setResultStatus("SUCCESS");
+                    print("[TEST EXECUTION RESULT] : SUCCESS");
+
+                else:
+
+                    tdkTestObj.setResultStatus("FAILURE");
+                    print("[TEST EXECUTION RESULT] : FAILURE");
+
+            else:
+
+                tdkTestObj.setResultStatus("FAILURE");
+                print("[TEST EXECUTION RESULT] : FAILURE");
+
+            ############################################################
+            # STEP 9 : Revert Device.Cellular.X_RDK_Enable to initial value
+            ############################################################
+
+            step = step + 1;
+
+            tdkTestObj = obj.createTestStep('TDKB_TR181Stub_Set');
+            tdkTestObj.addParameter(
+                "ParamName",
+                "Device.Cellular.X_RDK_Enable"
+            );
+            tdkTestObj.addParameter(
+                "ParamValue",
+                initialvalue
+            );
+            tdkTestObj.addParameter(
+                "Type",
+                "bool"
+            );
+
             tdkTestObj.executeTestCase(expectedresult);
+
             actualresult = tdkTestObj.getResult();
-            setvalue = tdkTestObj.getResultDetails();
-            print("Reverted the value of Device.Cellular.X_RDK_Enable to initial value");
+            revertDetails = tdkTestObj.getResultDetails();
+
+            print("TEST STEP %d: Revert Device.Cellular.X_RDK_Enable to initial value" %step);
+            print("EXPECTED RESULT %d: Device.Cellular.X_RDK_Enable should be restored to %s" %(step,initialvalue));
+            print("ACTUAL RESULT %d: %s" %(step,revertDetails));
+
+            if expectedresult in actualresult:
+
+                tdkTestObj.setResultStatus("SUCCESS");
+                print("[TEST EXECUTION RESULT] : SUCCESS");
+
+                sleep(10);
+
+                ############################################################
+                # STEP 10 : Verify Device.Cellular.X_RDK_Enable after Revert
+                ############################################################
+
+                step = step + 1;
+
+                tdkTestObj = obj.createTestStep('TDKB_TR181Stub_Get');
+                tdkTestObj.addParameter(
+                    "ParamName",
+                    "Device.Cellular.X_RDK_Enable"
+                );
+
+                tdkTestObj.executeTestCase(expectedresult);
+
+                actualresult = tdkTestObj.getResult();
+                revertValue = tdkTestObj.getResultDetails().strip();
+
+                print("TEST STEP %d: Verify Device.Cellular.X_RDK_Enable after revert" %step);
+                print("EXPECTED RESULT %d: Device.Cellular.X_RDK_Enable should be %s" %(step,initialvalue));
+                print("ACTUAL RESULT %d: Value is %s" %(step,revertValue));
+
+                if expectedresult in actualresult and revertValue == initialvalue:
+
+                    tdkTestObj.setResultStatus("SUCCESS");
+                    print("[TEST EXECUTION RESULT] : SUCCESS");
+
+                else:
+
+                    tdkTestObj.setResultStatus("FAILURE");
+                    print("[TEST EXECUTION RESULT] : FAILURE");
+
+            else:
+
+                tdkTestObj.setResultStatus("FAILURE");
+                print("[TEST EXECUTION RESULT] : FAILURE");
 
         else:
-            #Set the result status of execution
+
             tdkTestObj.setResultStatus("FAILURE");
-            #Get the result of execution
             print("[TEST EXECUTION RESULT] : FAILURE");
+
     else:
-        #Set the result status of execution
+
         tdkTestObj.setResultStatus("FAILURE");
-        #Get the result of execution
         print("[TEST EXECUTION RESULT] : FAILURE");
+
     obj.unloadModule("tdkbtr181");
     sysobj.unloadModule("sysutil");
+
 else:
+
     print("Failed to load module");
     sysobj.setLoadModuleStatus("FAILURE");
     obj.setLoadModuleStatus("FAILURE");
